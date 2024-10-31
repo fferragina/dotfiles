5cda8ecf90c9ddb87a572270d38850ad43cccfe1
fferragina <fferragina@gmail.com>
2024-10-31 01:33:42 +0100

    more dotfiles added

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "Tokyo Night",
+	font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
+	font_size = 11,
+	window_background_opacity = 0.9,
+}
+return config
